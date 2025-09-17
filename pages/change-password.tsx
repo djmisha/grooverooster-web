c380d9e11515e71191123a83fba67d6b25a285a3
Misha Osinovskiy <mosinovs@ccrn.com>
2025-09-17 07:50:11 -0700

    Comment ChangePassword page for now

--- pages/change-password.tsx
+++ pages/change-password.tsx
@@ -1,38 +1,38 @@
-import type { User } from "@supabase/supabase-js";
-import type { GetServerSidePropsContext } from "next";
-import ChangePassword from "../components/User/ChangePassword";
-import { createClient as createServerClient } from "../utils/supabase/server-props";
+// import type { User } from "@supabase/supabase-js";
+// import type { GetServerSidePropsContext } from "next";
+// import ChangePassword from "../components/User/ChangePassword";
+// import { createClient as createServerClient } from "../utils/supabase/server-props";
 
-interface ChangePasswordPageProps {
-  user: User;
-}
+// interface ChangePasswordPageProps {
+//   user: User;
+// }
 
-export default function ChangePasswordPage({ user }: ChangePasswordPageProps) {
-  return (
-    <>
-      <ChangePassword user={user} />
-    </>
-  );
-}
+// export default function ChangePasswordPage({ user }: ChangePasswordPageProps) {
+//   return (
+//     <>
+//       <ChangePassword user={user} />
+//     </>
+//   );
+// }
 
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  // Use server-side client for getServerSideProps
-  const supabase = createServerClient(context);
+// export async function getServerSideProps(context: GetServerSidePropsContext) {
+//   // Use server-side client for getServerSideProps
+//   const supabase = createServerClient(context);
 
-  const { data, error } = await supabase.auth.getUser();
+//   const { data, error } = await supabase.auth.getUser();
 
-  if (error || !data) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
-  }
+//   if (error || !data) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     };
+//   }
 
-  return {
-    props: {
-      user: data.user,
-    },
-  };
-}
+//   return {
+//     props: {
+//       user: data.user,
+//     },
+//   };
+// }
